93a590e37a80e1bf8a38513e2a1fae2a6521312f
Olivier Witteman <olivier@2001.net>
2021-05-15 21:39:15 +0200

    ugly testing

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dsmr_parser.objects import CosemObject, MBusObject, Telegram
 from dsmr_parser.parsers import TelegramParser
 import os
 import sys
+import time
 
 serial_reader = SerialReader(
     device='/dev/ttyUSB0',
@@ -15,13 +16,15 @@ serial_reader = SerialReader(
 
 # telegram = next(serial_reader.read_as_object())
 # print(telegram)
-verbose = False
+verbose, logging = False, False
 powerhist = []
 
 try:
     arg = sys.argv[1]
     if arg == '-v':
         verbose = True
+    elif arg == '-l':
+        logging = True
 except IndexError:
     arg = None
 
@@ -35,3 +38,11 @@ for telegram in serial_reader.read_as_object():
         powerhist = powerhist[-min(5, len(powerhist)):]
         data = f'\rCurrent power usage: {round(sum(powerhist)/len(powerhist))} W'
         print(data, end='')
+
+    if logging:
+        log_msg = ''
+        for attr, value in telegram:
+            log_msg += f',{telegram.P1_MESSAGE_TIMESTAMP.value},{attr},{value},\n'
+        print(os.path.abspath(os.path.dirname(__file__)))
+        # with open('')
+        time.sleep(5)
