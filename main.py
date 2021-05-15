b82f3196caed1ff01ef6d98bc814487acae49545
Olivier Witteman <olivier@2001.net>
2021-05-15 21:46:09 +0200

    ugly testing

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ for telegram in serial_reader.read_as_object():
     if logging:
         log_msg = ''
         for attr, value in telegram:
-            print(value.value)
-            log_msg += f',{telegram.P1_MESSAGE_TIMESTAMP.value},{attr},{value},\n'
+            if 'LOG' not in attr:
+                log_msg += f',{telegram.P1_MESSAGE_TIMESTAMP.value},{attr},{value.value}, {value.unit},\n'
         print(os.path.abspath(os.path.dirname(__file__)))
         with open(f'{os.path.abspath(os.path.dirname(__file__))}/P1_log.csv', 'a') as logfile:
             logfile.write(log_msg)
