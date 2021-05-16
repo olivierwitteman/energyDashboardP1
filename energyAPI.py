a595c596a7c3750143cbfad125cfb93c5bd0a1cd
Olivier Witteman <olivier@2001.net>
2021-05-16 16:32:31 +0200

    ugly testing

--- energyAPI.py
+++ energyAPI.py
@@ -15,8 +15,18 @@ def root():
     return {"msg": "test"}
 
 @app.get("/latest/")
-def return_latest():
+def return_metrics(metric: Optional[str] = None, n: Optional[int] = 1):
+    data = {}
+    lines = tail(f=f'{os.path.abspath(os.path.dirname(__file__))}/P1_log.csv', n=21*n).reverse()
+    last_sample = lines[0].split(",")[1]
 
-    lines = tail(f=f'{os.path.abspath(os.path.dirname(__file__))}/P1_log.csv', n=10)
-    print(lines)
+    dates = list(set([lambda d: d.split(',')[1] for d in lines]))
+
+    print(dates)
+
+    for line in lines:
+        pass
+
+        # if last_sample in line:
+
     return {"msg": lines}
