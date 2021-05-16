291e7487b0ce8309f1217f521a0c819aaf96a9c7
Olivier Witteman <olivier@2001.net>
2021-05-16 17:21:03 +0200

    ugly testing

--- energyAPI.py
+++ energyAPI.py
@@ -6,7 +6,7 @@ import subprocess
 app = FastAPI()
 
 def tail(f, n):
-    print(f'tail -n {n} {f}')
+    # print(f'tail -n {n} {f}')
     proc = subprocess.Popen(['tail', '-n', f'{n}', f], stdout=subprocess.PIPE)
     lines = proc.stdout.readlines()
     return lines
@@ -16,25 +16,26 @@ def root():
     return {"msg": "test"}
 
 @app.get("/latest/")
-def return_metrics(metric: Optional[str] = None, n: Optional[int] = 1):
-    data = {}
+def return_metrics(metric: Optional[str] = '', n: Optional[int] = 1):
+    data = {'data': [],
+            'date': []}
     lines = tail(f=f'{os.path.abspath(os.path.dirname(__file__))}/P1_log.csv', n=int(21*n))
     lines.reverse()
 
     dates = list(set([d.split(b',')[1] for d in lines]))[:n]
     #
-    print(dates)
+    # print(dates)
 
 
     for date in dates:
         keeplines = []
         for line in lines:
-            if all(cond in line for cond in date+bytes(metric)):
-                keeplines.append(line)
-        data['date'] = date
-        data['data'] = keeplines
+            if all(cond in line for cond in date+str.encode(metric)):
+                keeplines.append({f"{line.split(b',')[2]} [{line.split(b',')[4]}]": line.split(b',')[3]})
+        data['date'].append(date)
+        data['data'].append(keeplines)
 
 
     return {"msg": data}
 
-return_metrics(n=2)
+# print(return_metrics(n=2, metric='POWER_L1'))
