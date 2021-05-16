c9dd17b033f952e825d7cdb3665e20754a7d9e9f
Olivier Witteman <olivier@2001.net>
2021-05-16 19:41:38 +0200

    ugly testing

--- energyAPI.py
+++ energyAPI.py
@@ -21,7 +21,7 @@ def return_metrics(metric: Optional[str] = '', n: Optional[int] = 1):
     trimmedlines = []
     if metric != '':
         for line in lines:
-            if any(cond in line for cond in [str.encode(metric), b'TIMESTAMP']):
+            if any(cond in line for cond in [str.encode(metric)]):
                 trimmedlines.append(line)
     else:
         trimmedlines = lines
