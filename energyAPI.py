39ebdb791e2e63904aa28f7bd6469dfdf25a41ac
Olivier Witteman <olivier@2001.net>
2021-05-16 17:02:20 +0200

    ugly testing

--- energyAPI.py
+++ energyAPI.py
@@ -20,16 +20,21 @@ def return_metrics(metric: Optional[str] = None, n: Optional[int] = 1):
     data = {}
     lines = tail(f=f'{os.path.abspath(os.path.dirname(__file__))}/P1_log.csv', n=int(21*n))
     lines.reverse()
-    # print(lines)
-    # last_sample = lines[0].split(",")[1]
 
-    dates = list(set([d.split(',')[1] for d in lines]))
+    dates = list(set([d.split(b',')[1] for d in lines]))[:n]
     #
     print(dates)
 
-    for line in lines:
-        pass
 
-        # if last_sample in line:
+    for date in dates:
+        keeplines = []
+        for line in lines:
+            if all(cond in line for cond in date+bytes(metric)):
+                keeplines.append(line)
+        data['date'] = date
+        data['data'] = keeplines
 
-    return {"msg": lines}
+
+    return {"msg": data}
+
+return_metrics(n=2)
